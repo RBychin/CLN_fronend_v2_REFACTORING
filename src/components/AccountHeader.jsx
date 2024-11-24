293742ff551b29d1d51e15f912a3a7b2a54fee00
roman <r.bychine@gmail.com>
2024-11-24 11:49:12 +0300

    fix user pic

--- src/components/AccountHeader.jsx
+++ src/components/AccountHeader.jsx
@@ -1,6 +1,15 @@
 import {Config} from "../utills/config";
+import {useEffect, useState} from "react";
 
 export const AccountHeader = (props) => {
+
+    const [user, setUser] = useState(false);
+
+    useEffect(() => {
+        if (Config.tgWindow.initDataUnsafe.user) {
+            setUser(Config.tgWindow.initDataUnsafe.user);
+        }
+    }, [user])
 
     return (
 
@@ -8,10 +17,10 @@ export const AccountHeader = (props) => {
                         <span className='left-text'>
                             <div className='flex-start'>
                                 <img
-                                    src={Config.tgWindow && Config.tgWindow.initDataUnsafe && Config.tgWindow.initDataUnsafe.user && Config.tgWindow.initDataUnsafe.user.photo_url ? `${Config.tgWindow.initDataUnsafe.user.photo_url}` : "https://yt3.ggpht.com/ytc/AKedOLR7md9PKMjXdxlzQYIucyaNaQtXG0LB7WbcQV8N=s900-c-k-c0x00ffffff-no-rj"}
+                                    src={user && user.photo_url ? `${user.photo_url}` : "https://yt3.ggpht.com/ytc/AKedOLR7md9PKMjXdxlzQYIucyaNaQtXG0LB7WbcQV8N=s900-c-k-c0x00ffffff-no-rj"}
                                     className='userimage' alt='User'
                                 />
-                                <p className='username'>{Config.user.first_name}</p>
+                                <p className='username'>{user.first_name}</p>
                             </div>
                         </span>
                          <span className='right-text'>
